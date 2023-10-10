1f3f9bbb3a921eb01523c91e3928d5539dff9f92
MiranValincic <80262902+MiranValincic@users.noreply.github.com>
2023-10-10 15:20:32 +0200

    Added arrows

---
+++ app.js
@@ -0,0 +1,49 @@
+
+var swiper = new Swiper(".movies-slider", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+   navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+var swiper = new Swiper(".series-slider", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+   navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+var swiper = new Swiper(".music-slider", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+   navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+var swiper = new Swiper(".sports-slider", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
